b87c0c2bc11f6bdbf6bcc01a97b8e4a718ca2382
John Peter <jay3pee@gmail.com>
2020-05-08 11:05:14 +0100

    initial commit with complete structure

---
+++ animations.js
@@ -0,0 +1,11 @@
+var nav = document.querySelector('.nav-ul');
+var navlist = document.querySelector('.nav-list');
+// var stripe2 = document.querySelector('#stripe2');
+// var stripe3 = document.querySelector('#stripe3');
+
+// menu.addEventListener('click', displayMenu);
+function mobileMenu(menu) {
+    menu.classList.toggle('rotate');
+    nav.classList.toggle('nav-show');
+    navlist.classList.toggle('nav-show');
+}
