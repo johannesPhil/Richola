7f58748a456236a4ea7e5be5367cb9701994e9a4
John Peter <jay3pee@gmail.com>
2020-05-20 18:30:55 +0100

    Filled in the content

--- animations.js
+++ animations.js
@@ -1,5 +1,6 @@
 var nav = document.querySelector('.nav-ul');
 var navlist = document.querySelector('.nav-list');
+var navbar = document.querySelector('.nav-bar');
 // var stripe2 = document.querySelector('#stripe2');
 // var stripe3 = document.querySelector('#stripe3');
 
@@ -8,4 +9,5 @@ function mobileMenu(menu) {
     menu.classList.toggle('rotate');
     nav.classList.toggle('nav-show');
     navlist.classList.toggle('nav-show');
+    navbar.classList.toggle('align-start');
 }
